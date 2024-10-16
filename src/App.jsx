65dc1bfd62703c08c03b6df6b2673e40cea5a6e7
Md Masoom <mdmasoom9060@gmail.com>
2024-10-17 00:33:13 +0530

    updated files

--- src/App.jsx
+++ src/App.jsx
@@ -37,11 +37,7 @@ function App() {
 
   return (
     <TodoProvider value={{ todos, addTodo, updateTodo, deleteTodo, toggleComplete }}>
-      <div className="bg-[#172842] min-h-screen py-8 "
-      //   style={{
-      //     backgroundImage: `url('https://images.pexels.com/photos/19664569/pexels-photo-19664569/free-photo-of-portrait-of-man-in-blur-in-black-and-white.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1')`,
-      // }}
-      >
+      <div className="bg-transparent min-h-screen py-8 ">
         <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
           <h1 className="text-2xl font-bold text-center mb-8 mt-2">Manage Your Todos-Masoom</h1>
           <div className="mb-4">
